246e32981f1c9fa16b4bdb401a6e41ae9b362fe2
Jose Alfredo Alvarez Aldana <josealfredo1515@gmail.com>
2017-05-11 01:52:57 +0200

    Small changes so that root can find aws command.

--- main.sh
+++ main.sh
@@ -13,9 +13,9 @@
 # First, we get the instance ID, this IP is fixed, is not dependant on anything
 INSTANCE=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 # Then based on the Instance ID, we get all tags
-TAGS=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE" --output=text)
+TAGS=$(~/.local/bin/aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE" --output=text)
 
 # Then we select the tag we need
 FOO=$(echo "$TAGS" | grep Foo | cut -f5)
 
-echo $FOO >> /home/ubuntu/foo.txt
+echo $FOO > /home/ubuntu/foo.txt
