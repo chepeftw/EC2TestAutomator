fcb375599b9a096adc0dac4ef625fc80f6144baf
Jose Alfredo Alvarez Aldana <josealfredo1515@gmail.com>
2017-05-16 16:44:41 +0200

    Updating everything before starting.

--- updateService.sh
+++ updateService.sh
@@ -2,6 +2,7 @@
 
 # Update service but run it as root
 chmod +x /home/ubuntu/EC2TestAutomator/main.sh
+chmod +x /home/ubuntu/run.sh
 cp treesip.service /etc/systemd/system/
 chmod 664 /etc/systemd/system/treesip.service
 systemctl daemon-reload
