357f9f83a848128893d8ec2738ba236d1d885838
Jose Alfredo Alvarez Aldana <josealfredo1515@gmail.com>
2017-05-19 17:18:52 +0200

    Adding more tests.

--- multinodeTests.sh
+++ multinodeTests.sh
@@ -11,7 +11,7 @@ Timeout="$7"
 Speed="$8"
 Pause="$9"
 
-AMI_MULTI="ami-75b3d515"
+AMI_MULTI="ami-c3f197a3"
 ITYPE="t2.micro"
 
 case "$Nodes" in
@@ -25,7 +25,9 @@ case "$Nodes" in
     ;;
 "80") ITYPE="t2.large"
     ;;
-"90") ITYPE="t2.xlarge"
+"90") ITYPE="t2.large"
+    ;;
+"100") ITYPE="t2.xlarge"
     ;;
 esac
 
@@ -44,7 +46,7 @@ aws ec2 run-instances \
 
 # Timeout Test for 20 nodes
 COUNTER=1
-while [  $COUNTER -lt 5 ]; do
+while [  $COUNTER -lt 6 ]; do
     let SPEED=COUNTER*2
     # awsTreesip Name EmulationName Cycles Nodes Size TimeEmu Timeout Speed Pause InstanceType
     awsTreesip TreesipMulti MultiN_20_$SPEED 200 20 300 45 200 $SPEED 0
@@ -55,5 +57,6 @@ while [  $COUNTER -lt 5 ]; do
     awsTreesip TreesipMulti MultiN_70_$SPEED 200 70 550 95 200 $SPEED 0
     awsTreesip TreesipMulti MultiN_80_$SPEED 200 80 600 105 200 $SPEED 0
     awsTreesip TreesipMulti MultiN_90_$SPEED 200 90 650 115 200 $SPEED 0
+    awsTreesip TreesipMulti MultiN_100_$SPEED 200 100 650 125 200 $SPEED 0
 	let COUNTER=COUNTER+1
 done
--- queryResults3.js
+++ queryResults3.js
@@ -5,13 +5,18 @@ db.testcases.aggregate(
        $group:
          {
            _id: "$name",
-           minQuantity: { $min: "$convergence" },
-           maxQuantity: { $max: "$convergence" },
-           avgQuantity: { $avg: "$convergence" },
+           minConvergence: { $min: "$convergence" },
+           maxConvergence: { $max: "$convergence" },
+           avgConvergence: { $avg: "$convergence" },
            avgComputations: { $avg: "$computation" },
+           minConvergence2: { $min: "$convergence2" },
+           maxConvergence2: { $max: "$convergence2" },
+           avgConvergence2: { $avg: "$convergence2" },
+           avgComputations2: { $avg: "$computation2" },
            timeout: { $avg: "$timeout" },
            speed: { $avg: "$node_speed" },
            accuracy: { $avg: { $divide: [ "$computation", "$nodes" ] }},
+           accuracy2: { $avg: { $divide: [ "$computation2", "$nodes" ] }},
            runs: { $sum: 1 }
          }
      },
